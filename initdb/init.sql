2521e3b55df691b8889a18668ed551477de86157
danghohai2004 <dhhaics2004@gmail.com>
2025-11-09 16:59:38 +0700

    Update init.sql file

--- initdb/init.sql
+++ initdb/init.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE employees (
     emp_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE face_embeddings (
