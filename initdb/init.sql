b88b43182734cfae071163785c410e5c032fc2f8
danghohai2004 <dhhaics2004@gmail.com>
2026-01-08 01:06:27 +0700

    update init_sql file

--- initdb/init.sql
+++ initdb/init.sql
@@ -14,10 +14,15 @@ CREATE TABLE face_embeddings (
 
 CREATE TABLE attendance_logs (
     log_id SERIAL PRIMARY KEY,
-    emp_id INT NOT NULL, 
-    working_date DATE NOT NULL,    
-    checkin_time TIME,               
-    checkout_time TIME,                
-    working_duration INTERVAL,
-	FOREIGN KEY (emp_id) REFERENCES employees(emp_id) ON DELETE CASCADE
+    emp_id INT NOT NULL,
+    working_date DATE NOT NULL,
+    checkin_time TIMESTAMP NOT NULL,
+    checkout_time TIMESTAMP,
+    working_duration INTERVAL GENERATED ALWAYS AS
+        (checkout_time - checkin_time) STORED,
+    FOREIGN KEY (emp_id)
+        REFERENCES employees(emp_id)
+        ON DELETE CASCADE,
+    CONSTRAINT valid_attendance_time
+        CHECK (checkout_time IS NULL OR checkout_time > checkin_time)
 );
